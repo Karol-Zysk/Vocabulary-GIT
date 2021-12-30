40bec336e775fd885211b476fe4e0407ea39645b
Karol Zyśk <trener.karolzysk@gmail.com>
2021-12-30 01:42:08 +0100

    code refactorization

--- app.js
+++ app.js
@@ -16,10 +16,7 @@ const animalSound = document.querySelectorAll('.animalSound')
 const vehicleSound = document.querySelectorAll('.vehicleSound')
 const fruitSound = document.querySelectorAll('.fruitSound')
 const menu1 = document.getElementById('menu1')
-const menu = document.querySelector('.menu')
-const content = document.querySelector('.content')
 const cntImg = document.querySelectorAll('.cnt-img')
-const headerTxt = document.querySelectorAll('.header-txt')
 const footerSpan = document.querySelector('.footerSpan')
 const mobileButton = document.querySelector('.nav-box-3')
 const compButton = document.querySelector('.nav-box-2')
@@ -28,7 +25,7 @@ const swBtn = document.querySelector('.mobile-sw-btn')
 //asignment ways to open popup on mobile and PC
 if (window.matchMedia("(max-width: 500px)").matches) {
   mobileButton.addEventListener('click', () => {
-  openpopup()
+  openpopup(popup)
   })
 } else {
   compButton.addEventListener('click', () => {
@@ -58,24 +55,14 @@ function closepopup(popup) {
 
 //Animation navbar fix
 container.forEach((elem, index) => {
-    elem.style.animationName = 'to-grey'
-    elem.style.animationDuration = '0.5s'
-    elem.style.animationIterationCount = '1'
-    elem.style.animationDirection = 'alternate'
+    elem.style.animation = 'to-grey 0.5s alternate linear forwards'
     elem.style.animationDelay = `${(index/3) +0.3}s`
-    elem.style.animationTimingFunction = 'linear'
-    elem.style.animationFillMode = 'forwards'
 });
 
 // Header letters Animation
 headerBox.forEach((elem , index) => {
-    elem.style.animationName = 'Animals-animation'
-    elem.style.animationDuration = '1s'
-    elem.style.animationIterationCount = 'infinite'
-    elem.style.animationDirection = 'alternate'
+    elem.style.animationName = 'Animals-animation ease-in 1s alternate infinite'
     elem.style.animationDelay = `${(index/10)+0.1}s`
-    elem.style.animationTimingFunction = 'ease-in'
-    
 });
 
 let values = [3,0]
@@ -83,83 +70,94 @@ let values = [3,0]
 
   animalsButton.addEventListener('click',  animalTimeOut )
 
+  //Fetching info from Json Files to menu
+  function Fetch(index) {
+    
+    fetch('./animals.json')
+    .then(function  (response) {
+      return response.json();
+    })
+    .then(function (animals){
+      menu1.innerHTML =       `   <h2>${animals[index].animalTipe}</h2>
+      <hr size="5px" color="white">
+      <br>
+      <p class="paragraph"><span class="vspan">Name:</span> ${animals[index].animalName}</p>
+      <p class="paragraph"><span class="vspan">Max age</span>: ${animals[index].animalAge}</p>
+      <p class="paragraph"><span class="vspan">Place:</span> ${animals[index].animalPlace}</p>
+      <p class="paragraph"><span class="vspan">Sound:</span> ${animals[index].animalSound}</p><br>
+      <span class="vspan">Fun Fact:</span><br><hr size=3px color = white><p class="fact-paragraph"> ${animals[index].animalFact}</p>`
+    })
+  }
+
+  //Styling Elements while changing categories
+function categoryChange(elem, index) {
+  
+  mobileButton.style.pointerEvents ="none"
+  elem.style.animation = 'opacity 0.5s linear'
+  elem.classList.remove(`vehicle-box${index+1}`)
+  elem.classList.remove(`fruits-box${index+1}`)
+  elem.classList.remove('cnt-img')
+  elem.classList.add(`animals-box${index+1}`)
+  container.forEach(elem => {
+      elem.style.animation = 'to-transparent1 0.3s linear forwards alternate'
+      contain.style.backgroundImage = "url('./img/jungle.jpg')"
+      menu1.style.display = "block"
+      menu1.innerHTML = ''
+    document.querySelector('.nav-box-3').innerText = "Select an animal to hear it and display facts "
+
+  })
+}
+
+//Boxes conected to sounds
+
+function playSound(name, index) {
+  values.push(index)
+  let name
+  values.shift()
+  let myValue = values[`${values.length-2}`]
+  `${name}Sound`[myValue].pause();
+  `${name}Sound`[myValue].currentTime = 0  
+  `${name}Sound`[index].play()
+}
+
+//Adding scroll function and new switch category button to mobile version
+function mobileScrolladdBtn(elem) {
+  if (window.matchMedia("(max-width: 500px)").matches) {
+    elem.addEventListener('click', () => window.scrollTo({
+      top: 455,
+      behavior: 'smooth',
+    }));
+    swBtn.style.display = "grid"
+    swBtn.addEventListener('click', () => {
+    openpopup(popup)
+    })
+  }
+}
+ 
+
 
 function animalTimeOut() {
   footerSpan.innerText = "The expert in anything was once a begginer."
-document.querySelector(".header").style.backgroundImage = 'red'
-document.querySelector(".btn").innerText = "Switch"
+  document.querySelector(".btn").innerText = "Switch"
 contentBox.forEach((elem, index)=>{
     setTimeout(() => {
-      if (window.matchMedia("(max-width: 500px)").matches) {
-        elem.addEventListener('click', () => window.scrollTo({
-          top: 455,
-          behavior: 'smooth',
-        }));
-        swBtn.style.display = "grid"
-        swBtn.addEventListener('click', () => {
-
-          openpopup(popup)
-        })
-      }
-      mobileButton.style.pointerEvents ="none"
-        elem.style.animationName = 'opacity'
-        elem.style.animationDuration = '0.5s'
-        elem.style.animationTimingFunction = 'linear'
-        elem.classList.remove(`vehicle-box${index+1}`)
-        elem.classList.remove(`fruits-box${index+1}`)
-        elem.classList.remove('cnt-img')
-        elem.classList.add(`animals-box${index+1}`)
-        container.forEach(elem => {
-          elem.style.animationName = 'to-transparent1'
-          elem.style.animationDuration = '0.3s'
-          elem.style.animationDelay = '0s'
-          elem.style.animationTimingFunction = 'linear'
-          contain.style.backgroundImage = "url('./img/jungle.jpg')"
-          menu1.style.display = "block"
-          
-          menu1.innerHTML = ''
-          document.querySelector('.nav-box-3').innerText = "Select an animal to hear it and display facts "
-
-        });
-        
-      elem.onclick = function  playSound(){
+        mobileScrolladdBtn(elem)
+        categoryChange(elem, index)
+      }, 
+      (index+1)*200);
+      elem.onclick = function  soundAndInfo(){
         if (elem.classList.contains(`animals-box${index+1}`)) {
-          menu1.style.animationName = 'menu-change'
-          menu1.style.animationDuration = '0.5s'
-          menu1.style.animationFillMode = 'forwards'
-          menu1.style.animationIterationCount ='1'     
-
-          
-          fetch('./animals.json')
-.then(function  (response) {
-  return response.json();
-})
-.then(function (animals){
-  menu1.innerHTML =       `   <h2>${animals[index].animalTipe}</h2>
-  <hr size="5px" color="white">
-  <br>
-  <p class="paragraph"><span class="vspan">Name:</span> ${animals[index].animalName}</p>
-  <p class="paragraph"><span class="vspan">Max age</span>: ${animals[index].animalAge}</p>
-  <p class="paragraph"><span class="vspan">Place:</span> ${animals[index].animalPlace}</p>
-  <p class="paragraph"><span class="vspan">Sound:</span> ${animals[index].animalSound}</p><br>
-  <span class="vspan">Fun Fact:</span><br><hr size=3px color = white><p class="fact-paragraph"> ${animals[index].animalFact}</p>`
-})
-
-      values.push(index)
-      values.shift()
-      let myValue = values[`${values.length-2}`]
-      // value = 
-      animalSound[myValue].pause();
-      animalSound[myValue].currentTime = 0  
-      animalSound[index].play()
+          menu1.style.animation = 'menu-change 0.3s forwards'
+          Fetch(index)
+          playSound(animal, index)
         }
-      }   
-           }, 
-    (index+1)*200);})
-    
+      }       
+  })
   popup.classList.remove('active')
-  overlay.classList.remove('active')
+overlay.classList.remove('active')
 }
+
+
 
 vehicleButton.addEventListener('click',  vehicleTimeOut )
 function vehicleTimeOut() {
@@ -186,14 +184,7 @@ contentBox.forEach((elem, index)=>{
         elem.classList.remove('cnt-img')
         elem.classList.add(`vehicle-box${index+1}`)
         container.forEach(elem => {
-          // counter = counter + 0.5
-          elem.style.animationName = 'to-transparent2'
-          elem.style.animationDuration = '0.3s'
-          elem.style.animationIterationCount = '1'
-          elem.style.animationDirection = 'alternate'
-          elem.style.animationDelay = '0s'
-          elem.style.animationTimingFunction = 'linear'
-          elem.style.animationFillMode = 'forwards'
+          elem.style.animationName = 'to-transparent2  0.3s linear forwards alternate'
           contain.style.backgroundImage = "url('./img/road.jpg')"
           menu1.innerHTML = ''
           menu1.style.display = "block"
@@ -228,7 +219,6 @@ contentBox.forEach((elem, index)=>{
         values.push(index)
         values.shift()
         let myValue = values[`${values.length-2}`]
-        // value = 
         vehicleSound[myValue].pause();
         vehicleSound[myValue].currentTime = 0  
         vehicleSound[index].play()
@@ -270,14 +260,7 @@ contentBox.forEach((elem, index)=>{
        elem.classList.remove('cnt-img')
         elem.classList.add(`fruits-box${index+1}`)
         container.forEach(elem => {
-          // counter = counter + 0.5
-          elem.style.animationName = 'to-transparent3'
-          elem.style.animationDuration = '0.3s'
-          elem.style.animationIterationCount = '1'
-          elem.style.animationDirection = 'alternate'
-          elem.style.animationDelay = '0s'
-          elem.style.animationTimingFunction = 'linear'
-          elem.style.animationFillMode = 'forwards'
+          elem.style.animationName = 'to-transparent3 0.3s linear forwards alternate'
           contain.style.backgroundImage = "url('./img/fruits1.jpg')"
           menu1.innerHTML = ''
           menu1.style.display = "block"
@@ -312,7 +295,6 @@ contentBox.forEach((elem, index)=>{
           values.push(index)
           values.shift()
           let myValue = values[`${values.length-2}`]
-          // value = 
           fruitSound[myValue].pause();
           fruitSound[myValue].currentTime = 0  
           fruitSound[index].play()
